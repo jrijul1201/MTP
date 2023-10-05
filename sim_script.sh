8581f66cb5948463e67ec088f6538679828b172b
dipeshs809 <dipeshs809@gmail.com>
2023-10-05 18:55:55 +0530

    dont know , commit from terminal

--- sim_script.sh
+++ sim_script.sh
@@ -7,41 +7,44 @@
 # NODES=(1)
 # TCP_FLAVOUR=("TcpNewReno")
 # RTT=(10)
-NODES=(40 60 80)
-TCP_FLAVOUR=("TcpNewReno" "TcpBbr" "TcpCubic")
-RTT=(10 50 100 150 200)
+# NODES=(80)
+# TCP_FLAVOUR=("TcpNewReno" "TcpBbr" "TcpCubic")
+# RTT=(10 50 100 150 200)
 
 cd ../../
 
 
-# # first loop iterates over nodes
-for i in {0..3}
-do
-# # second loop iterates over TCP flavours
-for j in {0..3}
-# # third loop iterate over RTT
-
-do
-
-for k in {0..5}
-
-do
-./ns3 run tcp-dumbell-3 -- --numNodes=${NODES[$i]} --tcpVariant=${TCP_FLAVOUR[j]} --roundTripTime=${RTT[k]}
-# # mv {queue,queue${RTT[$j]}}.tr
-# # mv {queuebw,queuebw${RTT[$j]}}.tr
-# # mv {queuelost,queuelost${RTT[$j]}}.tr
-# # mv {queuesize,queuesize${RTT[$j]}}.tr
-# # mv {queuewin,queuewin${RTT[$j]}}
-# # moving the trace files to the directory associated with the buffer size 
-# # mv $buff queue${RTT[$j]}.tr queuebw${RTT[$j]}.tr queuelost${RTT[$j]}.tr queuelost1${RTT[$j]}.tr queuesize${RTT[$j]}.tr queuewin${RTT[$j]}
-echo "This config ran successfully:" ${NODES[$i]} ${TCP_FLAVOUR[j]} ${RTT[k]}
-done
-
-done
-
-done
-
-# for folder in results/*
+# # # first loop iterates over nodes
+# for i in {0..1}
 # do
-# echo ${folder} | python3 examples/tcp/plotScript.py
+# # # second loop iterates over TCP flavours
+# for j in {0..3}
+# # # third loop iterate over RTT
+
+# do
+
+# for k in {0..5}
+
+# do
+# ./ns3 run tcp-dumbell-3 -- --numNodes=${NODES[$i]} --tcpVariant=${TCP_FLAVOUR[j]} --roundTripTime=${RTT[k]}
+# # # mv {queue,queue${RTT[$j]}}.tr
+# # # mv {queuebw,queuebw${RTT[$j]}}.tr
+# # # mv {queuelost,queuelost${RTT[$j]}}.tr
+# # # mv {queuesize,queuesize${RTT[$j]}}.tr
+# # # mv {queuewin,queuewin${RTT[$j]}}
+# # # moving the trace files to the directory associated with the buffer size 
+# # # mv $buff queue${RTT[$j]}.tr queuebw${RTT[$j]}.tr queuelost${RTT[$j]}.tr queuelost1${RTT[$j]}.tr queuesize${RTT[$j]}.tr queuewin${RTT[$j]}
+# echo "This config ran successfully:" ${NODES[$i]} ${TCP_FLAVOUR[j]} ${RTT[k]}
 # done
+
+# done
+
+# done
+
+for folder in results/*
+do
+cd results/${folder}/
+sudo mkdir plots
+ls
+echo ${folder} | python3 examples/tcp/plotScript.py
+done
