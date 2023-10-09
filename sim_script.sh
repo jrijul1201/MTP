bbc8b086b74f6c5ba2417b00f569309492624fea
dipeshs809 <dipeshs809@gmail.com>
2023-10-10 00:54:52 +0530

    completed scripts

--- sim_script.sh
+++ sim_script.sh
@@ -4,35 +4,32 @@
 # SM Aug 2015
 # 2 factors: buffer size and RTT
 # generates tracefiles for each value of buffer size and RTT. All simulations corresponding to a given buffer size are clubbed in a directory named after the buffer size. 
-NODES=(1)
-TCP_FLAVOUR=("TcpNewReno")
-RTT=(10)
-# NODES=(80)
-# TCP_FLAVOUR=("TcpNewReno" "TcpBbr" "TcpCubic")
-# RTT=(10 50 100 150 200)
+# NODES=(1)
+# TCP_FLAVOUR=("TcpNewReno")
+# RTT=(10)
+NODES=(40 60 80)
+TCP_FLAVOUR=("TcpNewReno" "TcpBbr" "TcpCubic")
+RTT=(10 50 100 150 200)
 
 cd ../results/
 rm -rf *
 git add .
 git commit -m "cleaned results"
-git pull origin master
-rm -rf *
-git add .
-git commit -m "cleaned results"
+git push --force
 
 cd ../../
 
 
 # first loop iterates over nodes
-for i in {0..0}
+for i in {0..2}
 do
 # # second loop iterates over TCP flavours
-for j in {0..0}
+for j in {0..2}
 # # third loop iterate over RTT
 
 do
 
-for k in {0..0}
+for k in {0..4}
 
 do
 ./ns3 run tcp-dumbell-3 -- --numNodes=${NODES[$i]} --tcpVariant=${TCP_FLAVOUR[j]} --roundTripTime=${RTT[k]}
