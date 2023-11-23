fedfd21c81ba43f70f7bb70c174d3561d4109243
jrijul1201 <jrijul1201@gmail.com>
2023-11-23 12:53:01 +0530

    single bottleneck tcptype issue

--- rijul.sh
+++ rijul.sh
@@ -1,5 +1,5 @@
 NODES=(60)
-TCP_FLAVOUR=("TcpCubic" "TcpNewReno")
+TCP_FLAVOUR=("TcpNewReno" "TcpCubic")
 RTT=(10 200)
 THRESH=(true false)
 
@@ -22,7 +22,7 @@ do
             for l in {0..1}
             do
                 echo "Running this config:" ${NODES[$i]} ${TCP_FLAVOUR[j]} ${RTT[k]} ${THRESH[l]}
-                ./ns3 run parking-lot-topology -- --numNodes=${NODES[$i]} --tcpVariant=${TCP_FLAVOUR[$j]} --roundTripTime=${RTT[$k]} --thEnabled=${THRESH[$l]}
+                ./ns3 run single-bottleneck-dumbbell-topology -- --numNodes=${NODES[$i]} --tcpVariant=${TCP_FLAVOUR[$j]} --roundTripTime=${RTT[$k]} --withThresh=${THRESH[$l]}
                 echo "This config ran successfully:" ${NODES[$i]} ${TCP_FLAVOUR[j]} ${RTT[k]} ${THRESH[l]}
             done
         done
--- sync-loss/sync-loss.sh
+++ sync-loss/sync-loss.sh
@@ -16,15 +16,15 @@ folder_paths=(
     "/home/rijul/ns-allinone-3.36.1/ns-3.36.1/examples/results/60-TcpNewReno-200-withoutThresh/cwndTraces"
     "/home/rijul/ns-allinone-3.36.1/ns-3.36.1/examples/results/60-TcpNewReno-200-withThresh/cwndTraces"
 )
-filename_one="loss-events-one.dat"
+# filename_one="loss-events-one.dat"
 filename_half="loss-events-half.dat"
 
 for folder_path in "${folder_paths[@]}"; do
-    output_file_path_one="${folder_path}/${filename_one}"
+    # output_file_path_one="${folder_path}/${filename_one}"
     output_file_path_half="${folder_path}/${filename_half}"
     
     # Remove existing files
-    remove_existing_file "$output_file_path_one"
+    # remove_existing_file "$output_file_path_one"
     remove_existing_file "$output_file_path_half"
     
     # Iterate over each file in the folder
@@ -37,7 +37,7 @@ for folder_path in "${folder_paths[@]}"; do
             # Check if the filename matches the pattern "n{i}.dat"
             if [[ $filename =~ ^n[0-9]+\.dat$ ]]; then
                 # Run loss-events.py script on the current file with complete filepath
-                python3 loss-events.py "$file" "$output_file_path_one" --one
+                # python3 loss-events.py "$file" "$output_file_path_one" --one
                 python3 loss-events.py "$file" "$output_file_path_half" --half
                 
                 # echo "Processed $file"
@@ -50,5 +50,5 @@ for folder_path in "${folder_paths[@]}"; do
     # Log processing completion
     echo "Processed $folder_path"
     python3 compute-sync-metric.py "${output_file_path_half}" "${folder_path}" --half
-    python3 compute-sync-metric.py "${output_file_path_one}" "${folder_path}" --one
+    # python3 compute-sync-metric.py "${output_file_path_one}" "${folder_path}" --one
 done
