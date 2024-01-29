6aa918197c0c55785e92690f5afa57246384c145
jrijul1201 <jrijul1201@gmail.com>
2024-01-29 16:09:32 +0530

    sim script for static

--- run-sim.sh
+++ run-sim.sh
@@ -1,33 +1,43 @@
-NODES=(60)
-TCP_FLAVOUR=("TcpCubic" "TcpNewReno")
-RTT=(10 200)
+# NODES=(60)
+# TCP_FLAVOUR=("TcpCubic" "TcpNewReno")
+# RTT=(10 200)
 THRESH=(true false)
 
 cd ../results/
-rm -rf *
+# rm -rf *
 
 cd ../../
 
 
-# first loop iterates over nodes
-for i in {0..0}
+# # first loop iterates over nodes
+# for i in {0..0}
+# do
+#     # # second loop iterates over TCP flavours
+#     for j in {0..1}
+#     do
+#         # # third loop iterate over RTT
+#         for k in {0..1}
+#         do
+#             # # fourth loop iterate over Thresh
+#             for l in {0..1}
+#             do
+#                 echo "Running this config:" ${NODES[$i]} ${TCP_FLAVOUR[j]} ${RTT[k]} ${THRESH[l]}
+#                 ./ns3 run parking-lot-topology -- --numNodes=${NODES[$i]} --tcpVariant=${TCP_FLAVOUR[$j]} --roundTripTime=${RTT[$k]} --thEnabled=${THRESH[$l]}
+#                 echo "This config ran successfully:" ${NODES[$i]} ${TCP_FLAVOUR[j]} ${RTT[k]} ${THRESH[l]}
+#             done
+#         done
+#     done
+# done
+
+# # fourth loop iterate over Thresh
+for l in {0..1}
 do
-    # # second loop iterates over TCP flavours
-    for j in {0..1}
-    do
-        # # third loop iterate over RTT
-        for k in {0..1}
-        do
-            # # fourth loop iterate over Thresh
-            for l in {0..1}
-            do
-                echo "Running this config:" ${NODES[$i]} ${TCP_FLAVOUR[j]} ${RTT[k]} ${THRESH[l]}
-                ./ns3 run parking-lot-topology -- --numNodes=${NODES[$i]} --tcpVariant=${TCP_FLAVOUR[$j]} --roundTripTime=${RTT[$k]} --thEnabled=${THRESH[$l]}
-                echo "This config ran successfully:" ${NODES[$i]} ${TCP_FLAVOUR[j]} ${RTT[k]} ${THRESH[l]}
-            done
-        done
-    done
+    echo "Running this config:" ${THRESH[l]}
+    ./ns3 run static-routing-via-virtual -- --thEnabled=${THRESH[$l]}
+    echo "This config ran successfully:" ${THRESH[l]}
 done
+
+
 
 # bash plot-parking-lot.sh
 
@@ -35,5 +45,5 @@ cd examples/results/
 
 
 git add .
-git commit -m "parking-lot 100 100"
+git commit -m "static-routing-via-virtual for 80 nodes true and false"
 git push
