1740fc4e6c7d5ec51faf3c1c5575e48d206a396d
jrijul1201 <jrijul1201@gmail.com>
2024-01-29 16:03:23 +0530

    plots and stuff

---
+++ plot-static.py
@@ -0,0 +1,58 @@
+import os
+import random
+import sys
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+folder_path = "/home/rijul/ns-allinone-3.36.1/ns-3.36.1/examples/results/static-routing-via-virtual/WithThresh/146-TcpNewReno-100"
+
+plot_path = folder_path + "/plots"
+
+cwnd_folder = folder_path + "/cwndTraces"
+
+routerPaths = [
+    "router1",
+    "router2",
+    "router3",
+    "router4",
+    "router5",
+    "router6",
+]
+
+
+def plotAndSaveGraph(my_axs, xValues, yValues, label, title, xLabel, yLabel, filepath):
+    my_axs.plot(xValues, yValues, label=label)
+    my_axs.set_xlabel(xLabel)
+    my_axs.set_ylabel(yLabel)
+    my_axs.set_title(title)
+    my_axs.figure.savefig(filepath)  # Use figure to save the entire plot
+    plt.close()
+
+
+def readDatFileAndConvertToDataFrame(path, baseName):
+    df = pd.read_csv(path, delimiter=" ", header=None)
+    return df, plot_path + "/" + baseName + ".png"
+
+
+def plotQueues():
+    for routerNum in routerPaths:
+        df, path = readDatFileAndConvertToDataFrame(
+            folder_path + "/" + routerNum + "/queueSize.dat", "queueSize_" + routerNum
+        )
+        fig, axs = plt.subplots()  # Create a new figure and axis for each plot
+        plotAndSaveGraph(
+            axs,
+            np.array(df[0]),
+            np.array(df[1]),
+            "Queue -" + routerNum,
+            routerNum + " - Queue size vs Time(s)",
+            "Time (s)",
+            "Queue size",
+            path,
+        )
+
+
+# Call the plotQueues function
+plotQueues()
