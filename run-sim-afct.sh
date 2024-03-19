c1bafc0fb34355321d15d8f02aac7d12939b0b42
jrijul1201 <jrijul1201@gmail.com>
2024-03-19 14:05:53 +0530

    afct ready for running

--- run-sim-afct.sh
+++ run-sim-afct.sh
@@ -5,21 +5,21 @@ THRESH=(true false)
 RTT=(10 200)
 
 cd ../../
-# # second loop iterates over TCP flavours
-# for j in {0..1}
-# do
-#     # # third loop iterate over RTT
-#     for k in {0..1}
-#     do
-#         # # fourth loop iterate over Thresh
-#         for l in {0..1}
-#         do
-#             echo "Running this config:"  ${TCP_FLAVOUR[j]} ${RTT[k]} ${THRESH[l]}
-#             ./ns3 run afct-single -- --tcpVariant=${TCP_FLAVOUR[$j]} --roundTripTime=${RTT[$k]} --thEnabled=${THRESH[$l]}
-#             echo "This config ran successfully:"  ${TCP_FLAVOUR[j]} ${RTT[k]} ${THRESH[l]}
-#         done
-#     done
-# done
+# second loop iterates over TCP flavours
+for j in {0..1}
+do
+    # # third loop iterate over RTT
+    for k in {0..1}
+    do
+        # # fourth loop iterate over Thresh
+        for l in {0..1}
+        do
+            echo "Running this config:"  ${TCP_FLAVOUR[j]} ${RTT[k]} ${THRESH[l]}
+            ./ns3 run afct-single -- --tcpVariant=${TCP_FLAVOUR[$j]} --roundTripTime=${RTT[$k]} --thEnabled=${THRESH[$l]}
+            echo "This config ran successfully:"  ${TCP_FLAVOUR[j]} ${RTT[k]} ${THRESH[l]}
+        done
+    done
+done
 
 # # second loop iterates over TCP flavours
 for j in {0..1}
