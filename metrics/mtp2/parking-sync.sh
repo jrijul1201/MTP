3085010beb5ab9be7e883a7485a9012391f57940
jrijul1201 <jrijul1201@gmail.com>
2024-03-20 23:12:26 +0530

    parking

--- metrics/mtp2/parking-sync.sh
+++ metrics/mtp2/parking-sync.sh
@@ -2,10 +2,10 @@
 
 # List of folder paths
 folder_paths=(
-    "/home/Dipesh/ns-allinone-3.36.1/ns-3.36.1/examples/results/ppt-parking-lotWithoutThresh/60-TcpNewReno-10"
-    "/home/Dipesh/ns-allinone-3.36.1/ns-3.36.1/examples/results/ppt-parking-lotWithThresh/60-TcpNewReno-10"
-    "/home/Dipesh/ns-allinone-3.36.1/ns-3.36.1/examples/results/ppt-parking-lotWithThresh/60-TcpNewReno-200"
-    "/home/Dipesh/ns-allinone-3.36.1/ns-3.36.1/examples/results/ppt-parking-lotWithoutThresh/60-TcpNewReno-200"
+    "Dipesh/ns-allinone-3.36.1/ns-3.36.1/examples/results/ppt-parking-lotWithoutThresh/60-TcpNewReno-10"
+    "Dipesh/ns-allinone-3.36.1/ns-3.36.1/examples/results/ppt-parking-lotWithThresh/60-TcpNewReno-10"
+    "Dipesh/ns-allinone-3.36.1/ns-3.36.1/examples/results/ppt-parking-lotWithThresh/60-TcpNewReno-200"
+    "Dipesh/ns-allinone-3.36.1/ns-3.36.1/examples/results/ppt-parking-lotWithoutThresh/60-TcpNewReno-200"
 )
 
 my_script="parking-sync.py"
