2cd22e6dfd1632eeb5b2385ed9b79c94d7dd3ab3
jrijul1201 <jrijul1201@gmail.com>
2024-03-20 20:18:18 +0530

    stddev

--- metrics/afct/append-fct-data.py
+++ metrics/afct/append-fct-data.py
@@ -1,12 +1,15 @@
 import os
 import matplotlib.pyplot as plt
 import sys
+import statistics
+
 
 FCT_PATH = "/TcpLinuxReno/fct.dat"
 
 filePath = sys.argv[1]
 
 QTH_AQM = {2084: "Droptail", 15: "Threshold15", 100: "Threshold100"}
+
 
 def read_fct_dat_file(file_path):
     data = {}
@@ -23,25 +26,26 @@ def sort_dict(myDict):
     myKeys.sort()
     return {i: myDict[i] for i in myKeys}
 
+
 def calculate_set_stats(sets, folder_data):
     set_stats = {}
     for set_range in sets:
         set_name = f"{set_range[0]}-{set_range[1]}"
         set_stats[set_name] = {}
-        folder_values = [
-            folder_data[i] for i in range(set_range[0], set_range[1] + 1)
-        ]
+        folder_values = [folder_data[i] for i in range(set_range[0], set_range[1] + 1)]
         average = sum(folder_values) / len(folder_values)
         minimum = min(folder_values)
         maximum = max(folder_values)
+        stddev = statistics.stdev(folder_values)
         set_stats[set_name]["average"] = round(average, 2)
         set_stats[set_name]["minimum"] = round(minimum, 2)
         set_stats[set_name]["maximum"] = round(maximum, 2)
+        set_stats[set_name]["stddev"] = round(stddev, 2)
     return set_stats
 
 
 if __name__ == "__main__":
-    
+
     if len(sys.argv) != 2:
         print("Usage: python script.py input_file output_file folder_name")
         sys.exit(1)
@@ -61,15 +65,12 @@ if __name__ == "__main__":
         fileName = f"{QTH_AQM[int(qth)]}-{setName}.txt"
         path_to_append = save_path + fileName
         if not os.path.exists(path_to_append):
-            with open(path_to_append, 'w') as file:
-                file.write(f"rtt\tavg\tmin\tmax" + '\n')
+            with open(path_to_append, "w") as file:
+                file.write(f"rtt\tavg\tmin\tmax\tstddev" + "\n")
 
-        with open(path_to_append, 'a') as file:
+        with open(path_to_append, "a") as file:
             avg = setData["average"]
             mini = setData["minimum"]
             maxi = setData["maximum"]
-            file.write(f"{rtt}\t{avg}\t{mini}\t{maxi}" + '\n')
-
-
-        
-        
+            stddevi = setData["stddev"]
+            file.write(f"{rtt}\t{avg}\t{mini}\t{maxi}\t{stddevi}" + "\n")
